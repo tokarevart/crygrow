2ca9e73f7f61d5b367b868238edebcc7c72e30fd
Tokarev Artem <tokarev28.art@gmail.com>
2019-08-27 18:03:37 +0500

    update

--- automata-base.h
+++ automata-base.h
@@ -7,27 +7,36 @@
 
 namespace cgr {
 
+template<typename Cell>
+class cell_iterator : std::iterator<std::forward_iterator_tag, Cell> {
+public:
+    bool operator!=(cell_iterator const& other) const;
+    bool operator==(cell_iterator const& other) const;
+    typename cell_iterator::reference operator*() const;
+    cell_iterator& operator++();
+
+    cell_iterator(Cell* p);
+    // finish later
+
+private:
+    Cell* p;
+};
+
+// try change template Cell to cell_base class and make benchmarks
 template <std::size_t Dim, typename Cell>
 class automata_base {
 public:
     virtual Cell* get_cell(const spt::vec<Dim, std::size_t>& pos) const = 0;
-    virtual void reset_cell(const spt::vec<Dim, std::size_t>& pos, Cell* ptr = nullptr) = 0;
+    virtual void  reset_cell(const spt::vec<Dim, std::size_t>& pos, Cell* ptr = nullptr) = 0;
+
+    virtual cell_iterator begin_iter() const = 0;
+    virtual cell_iterator next_iter() const = 0;
+    virtual cell_iterator end_iter() const = 0;
 
     virtual bool stop_condition() const = 0;
-    virtual bool iterate() final {
-        for (auto& step : m_steps) 
-            step();
-        return !stop_condition();
-    }
-    virtual void add_step(std::function<bool()> step) final {
-        m_steps.push_back(step);
-    }
+    virtual bool iterate() = 0;
 
     virtual ~automata_base() {}
-
-
-private:
-    std::vector<std::function<bool()>> m_steps;
 };
 
 } // namespace cgr
--- crygrow.cpp
+++ crygrow.cpp
@@ -1,5 +1,47 @@
 ﻿#include <iostream>
 
+struct base {
+    int a = 0;
+
+    virtual void foo() {
+        std::cout << "base::foo" << std::endl;
+    }
+};
+
+struct derived0 : base {
+    int b = 1;
+
+    void foo() override final {
+        std::cout << "derived0::foo" << std::endl;
+    }
+
+    void bar() {
+        std::cout << "derived0::bar" << std::endl;
+    }
+};
+
+struct derived1 : base, derived0 {
+    int b = 1;
+    int c = 2;
+    
+    void bar() {
+        std::cout << "derived1::bar" << std::endl;
+    }
+
+    derived1() {
+        derived0::b++;
+        std::cout << "derived1::b " << b << std::endl;
+        std::cout << "derived0::b " << derived0::b << std::endl;
+
+        derived0::a++;
+        std::cout << "derived0::a " << derived0::a << std::endl;
+        std::cout << "base::a " << base::a << std::endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello World!\n";
+    base* ptr = new derived1;
+    ptr->a = 2;
+    std::cout << dynamic_cast<derived1*>(ptr)->derived0::a << ' ' << dynamic_cast<derived1*>(ptr)->b;
+    return 0;
 }

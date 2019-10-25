a60b28a360afa5b8a7abd0ec1cfb6300be6c571d
Tokarev Artem <tokarev28.art@gmail.com>
2019-10-25 05:17:19 +0500

    update

---
+++ cell-mutability.h
@@ -0,0 +1,46 @@
+// Copyright © 2019 Tokarev Artem. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+namespace cgr {
+
+enum class cell_mut_group {
+    mutable_only,
+    universal
+};
+
+enum class cell_mut {
+    mutable_cell,
+    constant_cell
+};
+
+
+template <cell_mut_group CellMut>
+class cell_base;
+
+
+template <>
+class cell_base<cell_mut_group::mutable_only> {
+public:
+    static constexpr auto mutability_group = cell_mut_group::mutable_only;
+};
+
+
+template <>
+class cell_base<cell_mut_group::universal> {
+public:
+    static constexpr auto mutability_group = cell_mut_group::universal;
+
+    cell_mut mutability() const {
+        return m_mut;
+    }
+
+    cell_base(cell_mut mut) : m_mut{mut} {}
+
+
+private:
+    cell_mut m_mut;
+};
+
+} // namespace cgr

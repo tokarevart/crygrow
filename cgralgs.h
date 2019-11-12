9f2d5c2a25a75b383edc1362d0fd5d6e592cb9e2
Tokarev Artem <tokarev28.art@gmail.com>
2019-11-12 21:09:04 +0500

    update

--- cgralgs.h
+++ cgralgs.h
@@ -18,24 +18,15 @@ std::int64_t offset(const spt::veci<Dim>& pos, const spt::vecu<Dim>& dim_lens) {
 template <std::size_t Dim>
 spt::vecu<Dim> upos(std::size_t offset, const spt::vecu<Dim>& dim_lens) {
     spt::vecu<Dim> res;
-    if constexpr (Dim == 2) {
-        auto x = dim_lens[0];
-        res[1] = offset / x;
-        res[0] = offset - x * res[1];
-
-    } else if constexpr (Dim == 3) {
-        auto x = dim_lens[0];
-        auto y = dim_lens[1];
-        auto xy = x * y;
-        res[2] = offset / xy;
-        auto t = offset - xy * res[2];
-        res[1] = t / x;
-        res[0] = t - x * res[1];
-
-    } else {
-        static_assert(false);
+    std::size_t t = offset;
+    for (std::size_t i = Dim - 1; i > 0; --i) {
+        std::size_t prod = dim_lens[0];
+        for (std::size_t j = 1; j < i; ++j)
+            prod *= dim_lens[j];
+        res[i] = t / prod;
+        t -= prod * res[i];
     }
-
+    res[0] = t;
     return res;
 }
 

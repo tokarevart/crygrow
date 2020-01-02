4b831c67a75db3a554573823454ff42a940bfef5
Tokarev Artem <tokarev28.art@gmail.com>
2020-01-02 08:51:57 +0500

    update

--- simple-geometry.h
+++ simple-geometry.h
@@ -21,6 +21,9 @@ public:
     // grains num then boundaries sorted
     using grouped2_grains_container = std::vector<std::vector<grains_container>>;
     using grouped2_offsets_container = std::vector<std::vector<offsets_container>>;
+    using vertices_container = offsets_container;
+    using edges_container = std::vector<offsets_container>;
+    using faces_container = std::vector<offsets_container>;
 
     bool grains_includes_unsorted(const grains_container& first, const grains_container& second) const {
         if (first.size() < second.size())
@@ -52,22 +55,102 @@ public:
         //
     }
 
-    offsets_container box_vertices() const {
-        std::size_t l0 = get_dim_lens()[0];
-        std::size_t l1 = get_dim_lens()[1];
-        std::size_t l2 = get_dim_lens()[2];
-        std::size_t l012 = num_cells();
-        std::size_t l01 = l0 * l1;
-        return { 
-                     0,              l0 - 1,  l01 - l0,  l01 - 1,
-            l012 - l01, l012 - l01 + l0 - 1, l012 - l0, l012 - 1
-        };
-    }
-    offsets_container box_edges() const {
+    template <std::size_t Dir>
+    std::size_t shift(std::size_t origin, std::size_t dist) const {
+        if constexpr (Dir == 0) 
+            return origin + dist;
 
-    }
-    offsets_container box_faces() const {
+        if constexpr (Dir == 1) 
+            return origin + get_dim_lens()[0] * dist;
 
+        if constexpr (Dir == 2) 
+            return origin + get_dim_lens()[1] * get_dim_lens()[0] * dist;
+    }
+    // exclusive dist_end
+    template <std::size_t Dir>
+    offsets_container shift(std::size_t origin, std::size_t dist_begin, std::size_t dist_end) const {
+        offsets_container res;
+        res.reserve(dist_end - dist_begin);
+        for (std::size_t i = dist_begin; i < dist_end; ++i)
+            res.push_back(shift<Dir>(origin, i));
+        return res;
+    }
+    template <std::size_t Dir>
+    offsets_container shift(offsets_container origins, std::size_t dist_begin, std::size_t dist_end) const {
+        offsets_container res;
+        res.reserve((dist_end - dist_begin) * origins.size());
+        for (auto origin : origins)
+            for (std::size_t i = dist_begin; i < dist_end; ++i)
+                res.push_back(shift<Dir>(origin, i));
+        return res;
+    }
+
+    /* make std::vector unique
+    std::unordered_set<std::size_t> s;
+    for (auto i : vec)
+        s.insert(i);
+    vec.assign(s.begin(), s.end());
+    */
+    template <std::size_t Dir>
+    std::size_t vshift(std::size_t origin) const {
+        return shift<Dir>(origin, get_dim_lens()[Dir] - 1);
+    }
+    template <std::size_t Dir>
+    offsets_container eshift(std::size_t vertex) const {
+        return shift<Dir>(vertex, 0, get_dim_lens()[Dir]);
+    }
+    template <std::size_t Dir>
+    offsets_container fshift(const offsets_container& edge) const {
+        return shift<Dir>(edge, 0, get_dim_lens()[Dir]);
+    }
+
+    vertices_container box_vertices() const {
+        vertices_container v(8);
+        v[0] = 0;
+        v[1] = vshift<0>(v[0]);
+        v[2] = vshift<1>(v[0]);
+        v[3] = vshift<0>(v[2]);
+        v[4] = vshift<2>(v[0]);
+        v[5] = vshift<0>(v[4]);
+        v[6] = vshift<1>(v[4]);
+        v[7] = vshift<0>(v[6]);
+        return v;
+    }
+    edges_container box_edges(const vertices_container& v) const {
+        edges_container e(12);
+        e[0] = std::move(eshift<0>(v[0]));
+        e[1] = std::move(eshift<1>(v[0]));
+        e[2] = std::move(eshift<0>(v[2]));
+        e[3] = std::move(eshift<1>(v[1]));
+        e[4] = std::move(eshift<0>(v[4]));
+        e[5] = std::move(eshift<1>(v[4]));
+        e[6] = std::move(eshift<0>(v[6]));
+        e[7] = std::move(eshift<1>(v[5]));
+        e[8] = std::move(eshift<2>(v[0]));
+        e[9] = std::move(eshift<2>(v[1]));
+        e[10] = std::move(eshift<2>(v[2]));
+        e[11] = std::move(eshift<2>(v[3]));
+        return e;
+    }
+    faces_container box_faces(const edges_container& e) const {
+        faces_container f(6);
+        f[0] = std::move(fshift<2>(e[0]));
+        f[1] = std::move(fshift<2>(e[1]));
+        f[2] = std::move(fshift<2>(e[2]));
+        f[3] = std::move(fshift<2>(e[3]));
+        f[4] = std::move(fshift<1>(e[4]));
+        f[5] = std::move(fshift<1>(e[0]));
+        return f;
+    }
+
+    offsets_container flatten(const std::vector<offsets_container>& conts) const {
+        offsets_container res(std::accumulate<std::size_t>(
+            conts.begin(), conts.end(), 0,
+            [](const offsets_container& e) { return e.size(); }));
+        for (auto& cont : conts)
+            for (auto off : cont)
+                res.push_back(off);
+        return res;
     }
 
     // pjoint is a point joint (or point boundary)
@@ -175,6 +258,8 @@ public:
         return res;
     }
 
+    //
+
     std::optional<std::string> is_global_max_order_overflow() const {
         for (std::size_t i = 0; i < num_cells(); ++i)
             if (get_grains(i).size() > 4)
@@ -182,16 +267,16 @@ public:
         return std::nullopt;
     }
     std::optional<std::string> is_box_vertices_max_order_overflow() const {
-
+        //
     }
     std::optional<std::string> is_box_edges_max_order_overflow() const {
-
+        //
     }
     std::optional<std::string> is_box_faces_max_order_overflow() const {
-
+        //
     }
     std::optional<std::string> is_exception() const {
-
+        //
     }
 
     simple_geometry_tool(const automata_type* automata)
